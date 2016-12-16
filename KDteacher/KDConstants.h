158adb3d8b271df5ee63a16692e4231bbfe8d5e0
yjjewg11 <41049630@qq.com>
2016-12-17 01:54:53 +0800

    https://mobile.umeng.com/images/pic/home/social/img-1.png"

--- KDteacher/KDConstants.h
+++ KDteacher/KDConstants.h
@@ -11,7 +11,8 @@
 @interface KDConstants : NSObject
 #define URL(baseURL, businessURL) [NSString stringWithFormat:@"%@%@", baseURL, businessURL];
 //umeng分享升级支持https。需要调整分享缩略图地址，必须是https 有效证书
-#define G_shareImageUrl @"https://mobile.umeng.com/images/pic/home/social/img-1.png"
+#define G_shareImageUrl @"https://www.wenjienet.com/px-rest/i/denglulogo.png";
+//#define G_shareImageUrl @"https://mobile.umeng.com/images/pic/home/social/img-1.png"
 #define G_baseServiceURL       @"https://www.wenjienet.com/px-rest/"      //正式
 //#define G_baseServiceURL @"http://120.25.212.44/px-rest/"                 //测试
 //#define G_baseServiceURL @"http://192.168.0.116:8080/px-rest/"
